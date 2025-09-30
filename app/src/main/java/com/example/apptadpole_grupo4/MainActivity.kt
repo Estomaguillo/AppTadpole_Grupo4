471bf0ca865548c73e0d6ff6abb5f535652f8dd0
Guillermo García Rojas <gu.garciar@duocuc.cl>
2025-09-30 20:41:45 -0300

    corrección
    se reemplaza el código por la copia que almacenamos en local

---
+++ app/src/main/java/com/example/apptadpole_grupo4/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.apptadpole_grupo4
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.apptadpole_grupo4.ui.theme.AppTadpole_Grupo4Theme
+import com.example.apptadpole_grupo4.ui.theme.HomeScreen
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppTadpole_Grupo4Theme {
+                /*Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }*/
+                HomeScreen()
+            }
+
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AppTadpole_Grupo4Theme {
+        Greeting("Android")
+    }
+}
